40ac34704c4ed875694b1471b170c535e8efa205
fengxiaohu.xiaohu <fengxiaohu.xiaohu@bytedance.com>
2022-03-21 21:38:10 +0800

    header: <style>(<inbplustree.h>): <add annotation to header files to make it easier to understand>

--- lib/bplustree.h
+++ lib/bplustree.h
@@ -75,47 +75,122 @@ static inline int list_is_last(struct list_head *link, struct list_head *head)
 #define list_for_each_safe(pos, n, head) \
         for (pos = (head)->next, n = pos->next; pos != (head); \
                 pos = n, n = pos->next)
-
+/**
+ * b plus tree basic node
+ * 
+ * 
+ */ 
 struct bplus_node {
+/**  type indicates leaf node or not
+ *     BPLUS_TREE_LEAF is 0 and  BPLUS_TREE_NON_LEAF is 1
+ */
         int type;
+/**     parent_key_idx: index of parent node */
         int parent_key_idx;
+/** piointer to parent node */
         struct bplus_non_leaf *parent;
+/** pointer to first node(head) in leaf linked list */
         struct list_head link;
+/** */
         int count;
 };
-
+/**  b plus tree non-leaf(internal)  node 
+ * non-leaf node need to carry children node information
+ * node contains only keys not key-value-pairs
+ 
+*/
 struct bplus_non_leaf {
+/**  type indicates leaf node or not
+ *     BPLUS_TREE_LEAF is 0 and  BPLUS_TREE_NON_LEAF is 1
+ */
         int type;
+/**     parent_key_idx: index of parent node */
         int parent_key_idx;
+/** piointer to parent node */
         struct bplus_non_leaf *parent;
+/** pointer to first node(head) in leaf linked list 
+*/
         struct list_head link;
+/**  number of child node */
         int children;
+/**  key array */
         int key[BPLUS_MAX_ORDER - 1];
+/** pointers to child node */
         struct bplus_node *sub_ptr[BPLUS_MAX_ORDER];
 };
-
+/**  b plus tree leaf node 
+ * leaf node need to carry key-value-pairs
+ 
+*/
 struct bplus_leaf {
+/**  type indicates leaf node or not
+ *     BPLUS_TREE_LEAF is 0 and  BPLUS_TREE_NON_LEAF is 1
+ */
         int type;
+/**     parent_key_idx: index of parent node */
         int parent_key_idx;
+/** piointer to parent node */
         struct bplus_non_leaf *parent;
+/** pointer to first node(head) in leaf linked list 
+*/
         struct list_head link;
+/** number of actual key-value pairs in leaf node */
         int entries;
+/**  key array */
         int key[BPLUS_MAX_ENTRIES];
+/**  val array */
         int data[BPLUS_MAX_ENTRIES];
-};
 
+};
+/** b plus tree structure */
 struct bplus_tree {
+/**  The actual number of children for a node, referred to here as order */
         int order;
+/** number of actual key-value pairs in tree */
         int entries;
+/** height of the tree */
         int level;
         struct bplus_node *root;
+/** double linked list to search leaf node */
         struct list_head list[BPLUS_MAX_LEVEL];
 };
-
+ /** print the whole tree for debugging
+   *  
+   * @param tree pointer to bplus tree
+   */
 void bplus_tree_dump(struct bplus_tree *tree);
+ /** return value acordding to key
+   *  
+   * @param tree pointer to bplus tree
+   * @param key  key in key-value pair
+   */
 int bplus_tree_get(struct bplus_tree *tree, key_t key);
+ /** insert key-value pair to tree
+   *  
+   * @param tree pointer to bplus tree
+   * @param key  key in key-value pair
+   * @param data value in key-value pair
+   
+   */
 int bplus_tree_put(struct bplus_tree *tree, key_t key, int data);
+
+ /** return 
+   *  
+   * @param tree pointer to bplus tree
+   * @param key1  key in key-value pair
+   * @param key2 value in key-value pair
+   
+*/
 int bplus_tree_get_range(struct bplus_tree *tree, key_t key1, key_t key2);
+
+ /** init b plus tree 
+  * @return a pointer to tree
+   *  
+   * @param order   The actual number of children for a node, referred to here as order 
+   * @param key1  key in key-value pair
+   * @param ksy2 value in key-value pair
+   
+*/
 struct bplus_tree *bplus_tree_init(int order, int entries);
 void bplus_tree_deinit(struct bplus_tree *tree);
 
