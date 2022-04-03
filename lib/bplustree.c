5bc2b957b8449bd7368750d92453fa292a6e1c47
fengxiaohu.xiaohu <fengxiaohu.xiaohu@bytedance.com>
2022-04-03 23:59:53 +0800

    feat:leaf_insert()

--- lib/bplustree.c
+++ lib/bplustree.c
@@ -1,10 +1,96 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-
-
 #include "bplustree.h"
 int bplus_tree_get(struct bplus_tree *tree, key_t key)
 {
 
+}
+
+/** 
+ * 
+ * */
+int leaf_insert(struct bplus_tree *tree,struct bplus_leaf *leafnode,key_t key,int data)
+{
+    
+}
+/** 
+ * 
+ * */
+static key_t key_binary_search(key_t *arr,int len,int target)
+{
+       
+}
+
+static struct bplus_leaf *leaf_new(void)
+{
+
+}
+
+ /** insert key-value pair to tree.
+  * if the tree is empty,we insert the key-value pair into root
+  * 
+  * if the tree is not empty: 
+  *     leaf node: find the location with binary search and insert
+  *     nonleaf node find suitable parent node, and insert it
+  *     
+  * 
+   * @return 0 if insert succeed , -1 if delete succeed
+   * @param tree pointer to bplus tree
+   * @param key  key in key-value pair
+   * @param data value in key-value pair
+   
+   */
+
+static int bplus_tree_insert(struct bplus_tree *tree,key_t key,int data)
+{
+    struct bplus_node *node = tree->root;
+    // 
+    while(node!=NULL)
+    {
+        if(is_leaf(node))
+        {
+            struct bplus_leaf *leafnode =  ( struct bplus_leaf *)node;
+            return leaf_insert(tree,leafnode,key,data);
+        }
+        else
+        {
+            struct bplus_non_leaf *non_leafnode = (struct bplus_non_leaf *)node;
+            int i = key_binary_search(non_leafnode->key,non_leafnode->children-1,key);
+            if(i>=0)
+            {
+                node = non_leafnode->sub_ptr[i+1];
+            }
+            {
+                i = -i-1;
+                node = non_leafnode->sub_ptr[i];// insert at tail
+            }
+            
+        }
+        
+    }
+    
+    struct bplus_leaf *root = leaf_new();
+    
+
+}
+static int bplus_tree_delete(struct bplus_tree *tree,key_t key)
+{
+    
+}
+
+int bplus_tree_put(struct bplus_tree *tree, key_t key, int data)
+{
+    if(data) 
+    {
+        return bplus_tree_insert(tree,key,data);
+    } 
+    else
+    {
+        return bplus_tree_delete(tree,key);
+    }
+}
+
+struct bplus_tree *bplus_tree_init(int order, int entries)
+{
+    
 }
